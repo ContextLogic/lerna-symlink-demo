f478c06f312cb61ddae7a6c020bd36423d74336c
kezhang-wish <kezhang@contextlogic.com>
2021-11-30 01:23:55 -0500

    setup.sh tested

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+ls
 cd project/symlink-project/node_modules/react && yarn link && cd -
 cd project/symlink-project/node_modules//react-dom && yarn link && cd -
 cd monorepo/packages/wps-button && yarn link && yarn install && yarn link react && yarn link react-dom && cd -
