f0bcf6c0a3469a90408a8ab1346cef3a471bc32a
kezhang-wish <kezhang@contextlogic.com>
2021-11-30 10:29:10 -0500

    yarn workspaces tested

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-ls
-cd project/symlink-project/node_modules/react && yarn link && cd -
-cd project/symlink-project/node_modules//react-dom && yarn link && cd -
-cd monorepo/packages/wps-button && yarn link && yarn install && yarn link react && yarn link react-dom && cd -
-cd project/symlink-project && yarn link wps-button
-yarn && yarn dev
+cd monorepo && yarn install && cd -
+cd monorepo/node_modules/react && yarn link && cd -
+cd monorepo/node_modules/react-dom && yarn link && cd -
+cd monorepo/packages/wps-button && yarn link && yarn build && cd -
+cd project/symlink-project && yarn link wps-button && yarn link react && yarn link react-dom
+yarn install && yarn dev
